65feb0f7c6df363e1cd1ff0aeb312753f1602826
venom0039 <tharindutd1998@gmail.com>
2021-06-20 20:27:36 +0530

    commit 20-06-2021 20.30PM

--- login.php
+++ login.php
@@ -4,38 +4,59 @@
         <title>Login</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"></script>
         <link rel="stylesheet" href="style/userStyle.css">
 
     </head>
 
     <body>
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar" href="image/icons/house.png"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>                        
-                    </button>
-                    <a class="navbar-brand">Macro Super Center</a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li><a href="home.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-                        <li><a href="search.php"><span class="glyphicon glyphicon-search"></span> Search</a></li>
-                        <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
-                        <li><a href="about.php"><span class="glyphicon glyphicon-info-sign"></span> About</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="signup.php"><span class="glyphicon glyphicon-plus-sign"></span> Sign up</a></li>
-                        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <br>
+    <?php 
+            session_start();
+            $system_userName="";
+            $system_userID;        
+        ?> 
+        <header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand">Macro Super Center</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="home.php">Home</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="search.php">Search</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="cart.php">Cart</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="about.php">About</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="logout.php"><?php if($system_userName==""){ echo "";} else {echo $system_userName;} ?></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="signup.php">Sign up</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="login.php">Log in</a>
+          </li>
+          </ul>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
 
         <?php
             if(isset($_POST['log_in'])) {
@@ -91,31 +112,33 @@
             }
         ?>
 
-        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br>
 
         <div class="container">
             <div id="form-group">
 
               <form action="login.php" method="post">
                 <div class="form-group">
-                <input type="text" id="email" name="email" placeholder = "email" required>
+                <input type="text" class="form-control" id="email" name="email" placeholder = "email" required>
                 </div>
                 <div class="form-group">
-                <input type="password" id="password" name="password" placeholder = "password" required> <br><br>
+                <input type="password" class="form-control" id="password" name="password" placeholder = "password" required> <br><br>
                 </div>
                 <div class="form-group">
-                <button type="Submit" class="btn btn-primary" name="log_in">Log in</button>
+                <button type="Submit" class="btn btn-primary btn-lg" name="log_in">Log in</button>
                 </div>
               </form>
             </div>
           </div>
 
-        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br>
 
         
 
-          <footer class="container-fluid text-center">
-            <p>Developed By <b>tharindu_johnson</b></p>
-        </footer>
+        <footer class="bg-dark text-center text-white">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <p>Developed By <b>tharindu_johnson</b></p>
+  </div>
+</footer>
     </body>
 </html>
